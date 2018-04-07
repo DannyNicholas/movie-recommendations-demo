43878d9e7adec9d7d869a997a2ccd2e95f3ba847
DannyNicholas <dannynicholas23@googlemail.com>
2018-04-07 10:47:12 +0100

    Initial implementation of controller and services

---
+++ src/main/java/com/danosoftware/servicemocks/controller/RecommendationController.java
@@ -0,0 +1,56 @@
+package com.danosoftware.servicemocks.controller;
+
+import com.danosoftware.servicemocks.dto.Movie;
+import com.danosoftware.servicemocks.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/movies")
+public class RecommendationController {
+
+    private final MovieService service;
+
+    @Autowired
+    public RecommendationController(MovieService service) {
+        this.service = service;
+    }
+
+    /**
+     * Get movie recommendations.
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Movie> recommend(
+            @RequestParam(value="genre", required = false) String genre,
+            @RequestParam(value="year", required = false) Integer year) {
+
+        return service.recommend(
+                Optional.ofNullable(genre),
+                Optional.ofNullable(year));
+    }
+
+    /**
+     * Get a movie using provided id.
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "/{movieId}")
+    public Movie getMovie(
+            @PathVariable Long movieId) {
+
+        return service.getMovie(movieId);
+    }
+
+    /**
+     * Add a new movie and return created id.
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus( HttpStatus.CREATED )
+    public Long add(
+            @RequestBody Movie movie) {
+
+        return service.addMovie(movie);
+    }
+}
