643c97e8e4a0bafac9167705cd737465fa765ae2
DannyNicholas <dannynicholas23@gmail.com>
2018-05-09 15:41:43 +0100

    Add stub movies dependency

--- src/main/java/com/danosoftware/movies/MovieRecommendationApplication.java
+++ src/main/java/com/danosoftware/movies/MovieRecommendationApplication.java
@@ -8,6 +8,8 @@ import com.danosoftware.movies.service.MovieService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,13 +17,14 @@ public class MovieRecommendationApplication {
 
 	public static void main(String[] args) {
 
-		MovieRepository repository = new StubMovieRepository();
+		List<Movie> stubMovies = stubMovies();
+		MovieRepository repository = new StubMovieRepository(stubMovies);
 		MovieService service = new FilterMovieService(repository);
 
 		test(service);
 	}
 
-	public static void test(MovieService service) {
+	private static void test(MovieService service) {
 
 		// log results
 		Logger LOGGER = LoggerFactory.getLogger(MovieRecommendationApplication.class);
@@ -32,5 +35,13 @@ public class MovieRecommendationApplication {
 
 		LOGGER.info("Movies found: {}", recommendations1.size());
 		recommendations1.stream().map(Movie::toString).forEach(LOGGER::info);
+	}
+
+	private static List<Movie> stubMovies() {
+		return Arrays.asList(
+				new Movie("Star Wars","Sci-Fi", LocalDate.of(1977, 5, 25)),
+				new Movie("The Godfather","Crime",LocalDate.of(1972, 3, 24)),
+				new Movie("Solaris","Sci-Fi",LocalDate.of(1972, 9, 26))
+		);
 	}
 }
--- src/main/java/com/danosoftware/movies/repository/StubMovieRepository.java
+++ src/main/java/com/danosoftware/movies/repository/StubMovieRepository.java
@@ -4,8 +4,6 @@ import com.danosoftware.movies.dto.Movie;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -16,16 +14,16 @@ public class StubMovieRepository implements MovieRepository {
 
     private static final Logger logger = LoggerFactory.getLogger(StubMovieRepository.class);
 
-    public StubMovieRepository() {
+    // stub recommended movies
+    private final List<Movie> stubMovies;
+
+    public StubMovieRepository(final List<Movie> stubMovies) {
+        this.stubMovies = stubMovies;
         logger.warn("Using Stub Movie Repository. Intended for local testing only.");
     }
 
     @Override
     public List<Movie> recommend() {
-        return Arrays.asList(
-                new Movie("Star Wars","Sci-Fi", LocalDate.of(1977, 5, 25)),
-                new Movie("The Godfather","Crime",LocalDate.of(1972, 3, 24)),
-                new Movie("Solaris","Sci-Fi",LocalDate.of(1972, 9, 26))
-        );
+        return stubMovies;
     }
 }
